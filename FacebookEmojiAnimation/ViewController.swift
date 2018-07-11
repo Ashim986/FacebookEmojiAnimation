3df338b9ba9fd122d6d4721b601ab8e6760c26bf
Ashim986 <ashim44@gmail.com>
2018-07-11 18:40:14 -0500

    Initial Commit

---
+++ FacebookEmojiAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookEmojiAnimation
+//
+//  Created by ashim Dahal on 7/11/18.
+//  Copyright © 2018 ashim Dahal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
